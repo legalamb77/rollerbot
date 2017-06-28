9ef2d4f0d5ccea3732087f80aa7891b8c0de08da
Theodore Morley <theodore.morley@mail.mcgill.ca>
2017-06-28 14:33:10 -0400

    Tested and fixed several bugs, basic functions now work. No support for additional arguments yet, but rolling dice works fine.

--- rollerbot.py
+++ rollerbot.py
@@ -1,5 +1,6 @@
 import praw
 import random
+import re
 
 #Bot input format: /roller followed by command.
 #Accepted commands:
@@ -11,16 +12,19 @@ import random
 ##-exploding: when a die is rolled that gets a max value, an additional die is rolled.
 #if /roller is called with badly a poorly formatted command, a small faq statement is printed showing the bot's input format.
 
-faq="ayylmao"
+faq="It looks like your comment isn't formatted properly for this bot. Please use the format xdy+z, where x, y, and z are integers."
 
 def rollDie(sidesDice):
     return random.randint(1,sidesDice)
 
 def rollDice(numDice,sidesDice,mod,modVal):
-    #returns the value of the rolled dice
+    #returns the value of the rolled dice tupled with the rolls that created that
     sum=0
+    rolls=[]
     while(numDice>0):
-        sum+=rollDie(sidesDice)
+        roll=rollDie(sidesDice)
+        rolls.append(roll)
+        sum+=roll
         numDice-=1
     if mod=="-":
         sum-=modVal
@@ -30,34 +34,39 @@ def rollDice(numDice,sidesDice,mod,modVal):
         sum*=modVal
     elif mod=="/":
         sum/=modVal
-    return sum
+    return (sum,rolls)
 
-with open(hiddenInfo) as f:
-    hiddenVars=f.readlines()
-hiddenVars=[x.strip() for x in hiddenVars]
+def main():
+    with open("hiddenInfo") as f:
+        hiddenVars=f.readlines()
+    hiddenVars=[x.strip() for x in hiddenVars]
 
-rollerbot=praw.Reddit(client_id=hiddenVars[0], client_secret=hiddenVars[1],
-                             password=hiddenVars[2], user_agent=hiddenVars[3],
-                                                  username=hiddenVars[4])
-subreddit=bot,subreddit('bottest')
+    rollerbot=praw.Reddit(client_id=hiddenVars[0], client_secret=hiddenVars[1],
+                                user_agent=hiddenVars[2], username=hiddenVars[3],
+                                                  password=hiddenVars[4])
+    subreddit=rollerbot.subreddit('bottest')
 
-comments=subreddit.stream.comments()
+    comments=subreddit.stream.comments()
 
-for comment in comments:
-    text=comment.body
-    if '/roller' in text:
-        expression='/roller\s*(\d\d?)d(\d{1,4})((\+|\-|\*|/)(\d{1,4}))?'
-        match=re.match(expression,text)
-        if match:
-            numberOfDice=int(match.group(1))
-            sidesOfDice=int(match.group(2))
-            modifier=None
-            modifierValue=0
-            if match.group(3):
-                modifier=match.group(4)
-                modifierValue=int(match.group(5))
-            value=rollDice(numberOfDice,sidesOfDice,modifier,modifierValue)
-            comment.reply(value)
-        else:
-            #Reply with faq
-            comment.reply(faq)
+    for comment in comments:
+        text=comment.body
+        if '/rollerbot' in text:
+            print("time to reply to somebody")
+            expression='/rollerbot\s*(\d\d?)d(\d{1,4})((\+|\-|\*|/)(\d{1,4}))?'
+            match=re.match(expression,text)
+            if match:
+                numberOfDice=int(match.group(1))
+                sidesOfDice=int(match.group(2))
+                modifier=None
+                modifierValue=0
+                if match.group(3):
+                    modifier=match.group(4)
+                    modifierValue=int(match.group(5))
+                value,rolls=rollDice(numberOfDice,sidesOfDice,modifier,modifierValue)
+                comment.reply("Hey there, it looks like you wanted me to roll some dice. You rolled "+str(rolls)+", for a total of: "+str(value))
+            else:
+                #Reply with faq
+                comment.reply(faq)
+
+if __name__ == '__main__':
+        main()
